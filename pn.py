5b8d2927e1d34273e2c133989ad768f352f81ef0
riyamanasa <72931821+riyamanasa@users.noreply.github.com>
2020-12-29 21:36:34 +0530

    creates positive numbers
    Input: list1 = [12, -7, 5, 64, -14]
    Output: 12, 5, 64

---
+++ pn.py
@@ -0,0 +1,9 @@
+list1 = []
+number = int(input("enter elements in list:"))
+for i in range(1,number+1):
+    value=int(input("enter elements %d in list:" %i))
+    list1.append(value)
+print("positive numbers are:")
+for j in range(number)
+    if(list1[j] >=0)
+       print(list1[j], end = " ") 
